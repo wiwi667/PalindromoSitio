aeb9f038ac96f16929ccf71bbba2215a46cb356e
Fernando Ordoñez <Fernando.Ordonez@portalesit.net>
2021-06-09 11:14:58 -0500

    TOOLBOX_API

--- src/constantes/index.js
+++ src/constantes/index.js
@@ -1,4 +1,4 @@
-//export const TOOLBOX_API = 'http://localhost:3010/'
-export const TOOLBOX_API = 'http://192.168.18.49:3010/'
+export const TOOLBOX_API = 'http://localhost:3010/'
+//export const TOOLBOX_API = 'http://192.168.18.49:3010/'
 
 export const INIT_STATE = { showProgressBar: false, error: '' }
