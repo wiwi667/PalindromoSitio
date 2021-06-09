6d4d51b870710e68f1f97ce5ecf2ee317c7b4cff
Fernando Ordoñez <Fernando.Ordonez@portalesit.net>
2021-06-09 11:14:25 -0500

    estilos

--- src/App.js
+++ src/App.js
@@ -27,12 +27,12 @@ const App = () => {
     <>
       <Navbar bg='dark' variant='dark' className='justify-content-center nav'>
         <Form className='form' onSubmit={e => { e.preventDefault() }}>
-          <Form.Row className='align-items-center form_row'>
+          <Form.Row className='align-items-center form_row my-1' sm={12}  lg={8} md={8}>
             <Col sm={12} lg={8} md={8} className='mx-1' space={2}>
               <FormControl type='text' placeholder='Insert text' onKeyPress={keyPresshandler} space={2} value={texto} onChange={(e) => setTexto(e.target.value)} />
             </Col>
             <Col sm={12} lg={5} md={3}>
-              <Button className='mx-3 boton' disabled={estado.showProgressBar} onClick={() => { invertir() }}>
+              <Button className='mx-3' disabled={estado.showProgressBar} onClick={() => { invertir() }}>
                 {estado.showProgressBar &&
                   <Spinner as='span' animation='grow' size='sm' role='status' aria-hidden='true' />
                 }
@@ -46,12 +46,14 @@ const App = () => {
         <Container className='row_container'>
           <Row className='justify-content-md-center my-1'>
             {estado.error &&
-              <Alert variant={'danger'}>
-                {estado.error}
-              </Alert>
+              <InputGroup className='mb-3'>
+                <Alert variant={'danger'}>
+                  <b className={'mx-1'}>Error:</b>{estado.error}
+                </Alert>
+              </InputGroup>
             }
             {lista.map((x, ix) => (
-              <Col xs lg='12' key={ix}>
+              <Col xs={12} lg={12} key={ix}>
                 <InputGroup className='mb-3'>
                   <FormControl defaultValue={x.text} value={x.text} title={x.textoOriginal} aria-describedby='basic-addon2' />
                   {x.palindrome &&
