bcfafdc8b49209bd2abf112a26f6c365e7b395c5
Fernando Ordoñez <Fernando.Ordonez@portalesit.net>
2021-06-09 21:54:42 -0500

    palindromo false

--- src/comunication/__test__/index.test.js
+++ src/comunication/__test__/index.test.js
@@ -11,3 +11,9 @@ test('Debe validar que sea un palindromo', async () => {
     const data = await invertirTexto('qazaq')
     expect(data.palindrome).toBe(true)
 })
+
+test('Debe validar que no sea un palindromo', async () => {
+    expect.assertions(1)
+    const data = await invertirTexto('qaz123')
+    expect(data.palindrome).toBe(false)
+})
